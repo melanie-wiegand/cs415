82da5b6a31e36ebd904bb73f4038eac1bd55cb72
melanie-wiegand <mwiegan6@uoregon.edu>
2025-04-18 12:06:52 -0700

    lab3

--- lab2/main.c
+++ lab2/main.c
@@ -3,16 +3,35 @@
 
 int main(int argc, char* argv[])
 {
-    // if no flags, start interactive mode
-    if (argc == 1) 
+    int filemode = 0;
+
+    // if -f flag is used, start file mode
+    if (argc == 3 && strcmp(argv[1], "-f") == 0)
     {
-        printf("Interactive mode:\n");
-    }
-    else if (argc == 3 && strcmp(argv[1], "-f") == 0)
-    {
-        char* FILE = argv[2];
+        // update file mode bool
+        filemode = 1;  
         printf("File mode:\n");
+
+        // open provided file
+        FILE *file = fopen(argv[2], "r");
+        if (file == NULL)
+        {
+            perror("Could not open file: %s\n", argv[2]);
+            return 1;
+        }
     }
+
+    // if no flags, start interactive mode
+    else if (argc == 1) 
+    {
+        // keep filemode bool at 0
+        printf("Interactive mode:\n");
+
+        // collect input from user
+        input = stdin;  
+    }
+
+    // otherwise, give error message
     else 
     {
         fprintf(stderr, "Usage:\n");
@@ -20,5 +39,11 @@ int main(int argc, char* argv[])
         fprintf(stderr, "  %s -f <file> (for file mode)\n", argv[0]);
         return 1;
     }
+
+    if (filemode)
+    {
+        fclose(file);
+    }
+
     return 0;
 }
