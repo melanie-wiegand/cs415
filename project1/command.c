fa9fb3b4311d27df9e4ab456268cfd7ecbcaf238
melanie-wiegand <mwiegan6@uoregon.edu>
2025-04-24 17:31:42 -0700

    removed newline after cat

--- project1/command.c
+++ project1/command.c
@@ -268,7 +268,7 @@ void displayFile(char *filename)
         }
         textsize = read(src, buffer, 100000);
     }
-    write(STDOUT_FILENO, "\n", 1);
+    // write(STDOUT_FILENO, "\n", 1);
 
     close(src);
 } 
