01b663ffbfe35eae6545ca7042c163730fab0b9a
melanie-wiegand <mwiegan6@uoregon.edu>
2025-05-10 15:30:02 -0700

    lab5

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <string.h>
 
 void signaler(pid_t* pid_ary, int size, int signal);
 
@@ -23,6 +24,10 @@ int main(int argc, char* argv[])
 	int size = atoi(argv[1]);
 
 	pid_t *pid_ary = (pid_t *)malloc(sizeof(pid_t) * size);
+	if (pid_ary == NULL) {
+        perror("malloc failed");
+        exit(-1);
+    }
 
 	char *args[] = {"./iobound", "-seconds", "4", 0};
 
@@ -31,11 +36,13 @@ int main(int argc, char* argv[])
 	int sig;
 
 	// create an empty sigset_t
+	sigemptyset(&sigset);
 
 	// use sigaddset() to add the SIGUSR1 signal to the set
+	sigaddset(&sigset, SIGUSR1);
 
 	// use sigprocmask() to add the signal set in the sigset for blocking
-
+	sigprocmask(SIG_BLOCK, &sigset, NULL);
 	
 	for(int i = 0; i < size; i++)
 	{
@@ -45,31 +52,46 @@ int main(int argc, char* argv[])
 		if(pid_ary[i] == 0)
 		{
 			// print: Child Process: <pid> - Waiting for SIGUSR1…
-
-			// wait for the signal
+			printf("Child Process: %d - Waiting for SIGUSR1...\n", getpid());
 			
+			// wait for the signal
+			sigwait(&sigset, &sig);
+
 			// print: Child Process: <pid> - Received signal: SIGUSR1 - Calling exec().
+            printf("Child Process: %d - Received signal: SIGUSR1 - Calling exec().\n", getpid());
 
 			// call execvp with ./iobound like in lab 4
+			execvp(args[0], args);
 
-			exit(0);
+			//if unsuccessful
+			perror("execvp failed");
+            exit(EXIT_FAILURE);
 
 		}
 		else if (pid_ary[i] < 0) {
 			// fork failed error
+			perror("fork failed");
+            free(pid_ary);
 			exit(-1);
 		}
 	}
 	
 	// send SIGUSR1 
+	signaler(pid_ary, size, SIGUSR1);
 
 	// send SIGSTOP 
+	signaler(pid_ary, size, SIGSTOP);
 
 	// send SIGCONT
+	signaler(pid_ary, size, SIGCONT);
 
 	// send SIGINT
+	signaler(pid_ary, size, SIGINT);
 
 
+	for (int i = 0; i < size; i++) {
+        waitpid(pid_ary[i], NULL, 0);
+    }
 
 	free(pid_ary);
 	
@@ -79,13 +101,18 @@ int main(int argc, char* argv[])
 void signaler(pid_t* pid_ary, int size, int signal)
 {
 	// sleep for three seconds
+	sleep(3);
 
 	for(int i = 0; i < size; i++)
 	{
-		print: Parent process: <pid> - Sending signal: <signal> to child process: <pid>
+		// print: Parent process: <pid> - Sending signal: <signal> to child process: <pid>
+        printf("Parent process: %d - Sending signal: %s to child process: %d\n", getpid(), strsignal(signal), pid_ary[i]);
 
 		// send the signal
-
+		if (kill(pid_ary[i], signal) == -1) 
+		{
+            perror("kill failed");
+        }
 
 	}
 }
