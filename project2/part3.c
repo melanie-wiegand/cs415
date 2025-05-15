6c86ceff8e49c42fd7e1d19ed360c5be84ec9104
melanie-wiegand <mwiegan6@uoregon.edu>
2025-05-15 16:36:41 -0700

    Update part3.c

--- project2/part3.c
+++ project2/part3.c
@@ -60,7 +60,7 @@ int main(int argc, char* argv[])
     sigprocmask(SIG_BLOCK, &sigset, NULL);
 
     // if context switch
-    signal(SIGALRM, alarm_handle);
+    // signal(SIGALRM, alarm_handle);
 
     while (1) 
     {
