a9bf5e5707e518b76b1cd6ef0b46ce9c79ee805a
melanie-wiegand <mwiegan6@uoregon.edu>
2025-06-05 17:09:35 -0700

    Update park.c

--- project3/part2/park.c
+++ project3/part2/park.c
@@ -499,9 +499,9 @@ int main(int argc, char* argv[])
     for (int i = 0; i < n; ++i) 
     {
         pass_ids[i] = i + 1;
-        if (n % 5 == 0)
+        if (n % 6 == 5)
         {
-            // every five passengers, wait longer (mimicking example behavior)
+            // every few passengers, wait longer (mimicking example behavior)
             randsleep(1, 5);
         }
         else
