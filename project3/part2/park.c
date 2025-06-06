086b72bda96d0f4bbda97a9e22667574a18e2f91
melanie-wiegand <mwiegan6@uoregon.edu>
2025-06-06 13:37:21 -0700

    make sure cars can unload at same time

--- project3/part2/park.c
+++ project3/part2/park.c
@@ -44,6 +44,9 @@ int current_car = 0;
 int ride_queue[MAX_PASSENGERS];
 int ride_front = 0;
 int ride_rear = 0;
+pthread_mutex_t unload_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t unload_cond = PTHREAD_COND_INITIALIZER;
+int next_unload = 0;
 
 // ticket ordering + queue
 pthread_mutex_t ticket_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -51,6 +54,7 @@ pthread_cond_t ticket_cond = PTHREAD_COND_INITIALIZER;
 int ticket_queue[MAX_PASSENGERS];
 int ticket_front = 0; 
 int ticket_rear = 0;
+
 
 // car struct
 typedef struct
@@ -233,7 +237,7 @@ void *passenger_routine(void *arg)
 
         print_time("is exploring the park...", subject, pid);
         // rand time to explore
-        randsleep(1, 10);
+        randsleep(1, 8);
         if (time_up) 
         {
             return NULL;
@@ -448,11 +452,23 @@ void *car_routine(void *arg)
         // reset boarded count
         car->boarded_count = 0;
 
+        // wait for turn to unload
+        pthread_mutex_lock(&unload_mutex);
+        while (cid != next_unload) 
+        {
+            pthread_cond_wait(&unload_cond, &unload_mutex);
+        }
+
         // signal end
         pthread_mutex_lock(&mutex);
         print_time("invoked unload()", subject, cid + 1);
         pthread_cond_broadcast(&car->ride_done);
         pthread_mutex_unlock(&mutex);
+
+        // next car's turn to unload
+        next_unload = (next_unload + 1) % c;
+        pthread_cond_broadcast(&unload_cond);
+        pthread_mutex_unlock(&unload_mutex);
     }
 
     return NULL;
@@ -462,7 +478,7 @@ void *car_routine(void *arg)
 int main(int argc, char* argv[])
 {
     int opt;
-    
+
     // check for flags
     while ((opt = getopt(argc, argv, "n:c:p:w:r:h")) != -1)
     {
